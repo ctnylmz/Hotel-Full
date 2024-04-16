88efcd5befeafcb48520e1bcb29810d6066ac3ea
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2024-04-16 11:46:42 +0300

    Create JWT

---
+++ WebApiJWT/Controllers/DefaultController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ZWebApiJWT.Models;
+
+namespace ZWebApiJWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+
+        [HttpGet("[action]")]
+        public IActionResult Everyone()
+        {
+            return Ok(new CreateToken().TokenCreate());
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult Admin()
+        {
+            return Ok(new CreateToken().TokenCreateAdmin());
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet("[action]")]
+        public IActionResult AdminToLogin()
+        {
+            return Ok("Admin Hoşgeldiniz");
+        }
+    }
+}
---
+++ WebApiJWT/Models/CreateToken.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ZWebApiJWT.Models
+{
+    public class CreateToken
+    {
+        public string TokenCreate()
+        {
+            var bytes = Encoding.UTF8.GetBytes("uyN8zrP6ucL3d74u@vbT#Q8*DYBdKfXv");
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            JwtSecurityToken token = new JwtSecurityToken
+                (
+                issuer: "http://localhost",
+                audience: "http://localhost",
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: credentials
+                );
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(token);
+        }
+
+        public string TokenCreateAdmin()
+        {
+            var bytes = Encoding.UTF8.GetBytes("uyN8zrP6ucL3d74u@vbT#Q8*DYBdKfXv");
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            List<Claim> claims = new List<Claim>()
+    {
+        new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+        new Claim(ClaimTypes.Role, "Admin") // Doğru rol burada belirtiliyor
+    };
+
+            JwtSecurityToken token = new JwtSecurityToken
+            (
+                issuer: "http://localhost",
+                audience: "http://localhost",
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddDays(1),
+                claims: claims, // claims parametresi eklendi
+                signingCredentials: credentials
+            );
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(token);
+        }
+    }
+}
