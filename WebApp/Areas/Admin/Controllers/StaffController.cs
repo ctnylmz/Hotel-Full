b8b9e5209799322babe07bf1618e53290e3766ad
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2024-04-17 13:21:51 +0300

    Staff Create To About Staff Count

--- WebApp/Areas/Admin/Controllers/StaffController.cs
+++ WebApp/Areas/Admin/Controllers/StaffController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Text;
 using WebApp.Areas.Admin.Models;
+using WebApp.Models;
 
 namespace WebApp.Areas.Admin.Controllers
 {
@@ -35,7 +37,7 @@ namespace WebApp.Areas.Admin.Controllers
 
 
         [HttpGet]
-        public IActionResult Add()
+        public async Task<IActionResult> Add()
         {
             return View();
         }
@@ -51,13 +53,14 @@ namespace WebApp.Areas.Admin.Controllers
 
             if (responseMessage.IsSuccessStatusCode)
             {
+                await UpdateAboutWithStaffCountAsync();
                 return RedirectToAction("Index");
             }
 
             return View();
         }
 
-    
+
 
         public async Task<IActionResult> Delete(int id)
         {
@@ -75,6 +78,7 @@ namespace WebApp.Areas.Admin.Controllers
 
                 if (responseDelete.IsSuccessStatusCode)
                 {
+                    await UpdateAboutWithStaffCountAsync();
                     return RedirectToAction("Index");
                 }
             }
@@ -118,9 +122,32 @@ namespace WebApp.Areas.Admin.Controllers
             return View();
         }
 
+        private async Task UpdateAboutWithStaffCountAsync()
+        {
+            var httpClient = _httpClientFactory.CreateClient();
 
+            // Personel sayısını al
+            var staffResponse = await httpClient.GetAsync("https://localhost:7199/api/Staff/GetList");
+            var staffData = await staffResponse.Content.ReadAsStringAsync();
+            var staffList = JsonConvert.DeserializeObject<List<StaffViewModel>>(JObject.Parse(staffData)["data"].ToString());
+            var staffCount = staffList.Count;
 
+            // About verilerini al ve ilk öğeye personel sayısını ekle
+            var aboutResponse = await httpClient.GetAsync("https://localhost:7199/api/About/GetList");
+            var aboutData = await aboutResponse.Content.ReadAsStringAsync();
+            var aboutList = JsonConvert.DeserializeObject<List<AboutViewModel>>(JObject.Parse(aboutData)["data"].ToString());
+            var firstAbout = aboutList.FirstOrDefault();
 
+            if (firstAbout != null)
+            {
+                firstAbout.staffCount = staffCount;
+                // About verilerini güncelle
+                var StaffUpdatejsonData = JsonConvert.SerializeObject(firstAbout);
+                var StaffUpdatestringContent = new StringContent(StaffUpdatejsonData, Encoding.UTF8, "application/json");
+                var StaffUpdateresponseMessage = await httpClient.PostAsync("https://localhost:7199/api/About/Update", StaffUpdatestringContent);
+            }
 
+        }
     }
+
 }
