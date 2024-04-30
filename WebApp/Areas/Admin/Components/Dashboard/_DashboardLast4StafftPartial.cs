8d2ee4ca3047f9c76af4d738b8de8330aad58ba4
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2024-04-30 18:40:52 +0300

    Admin Create Dashboard Last 4 Stafft

---
+++ WebApp/Areas/Admin/Components/Dashboard/_DashboardLast4StafftPartial.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Text;
+using WebApp.Areas.Admin.Models;
+using WebApp.Models;
+
+namespace WebApp.Areas.Admin.Components.Dashboard
+{
+    public class _DashboardLast4StafftPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DashboardLast4StafftPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7199/api/Staff/GetList");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<StaffViewModel>>(JObject.Parse(jsonData)["data"].ToString());
+
+                var lastFourStaff = values.Skip(Math.Max(0, values.Count - 4)).ToList();
+
+                return View(lastFourStaff);
+            }
+
+            return View();
+
+        }
+    }
+   
+}
